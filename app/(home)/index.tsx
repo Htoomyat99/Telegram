87c4110c6820d277f7b80c5e48d97432fe64ada3
Htoo Myat Lwin <htoomyatlwin@Htoos-MacBook-Air.local>
2024-07-13 23:26:35 +0630

    add expo router and setUp router

---
+++ app/(home)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MainTab = () => {
+  return (
+    <View>
+      <Text>index Main Tabs</Text>
+    </View>
+  );
+};
+
+export default MainTab;
